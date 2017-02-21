e16dabdb0dfa42463fc74fc84cf229d3f8cc9df5
Filipe Belatti <fibelatti@gmail.com>
2017-02-21 17:55:23 +0100

    Creating models for the API response.

---
+++ app/src/main/java/com/fibelatti/nytimes/models/Person.java
@@ -0,0 +1,76 @@
+
+package com.fibelatti.nytimes.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Person {
+
+    @SerializedName("firstname")
+    @Expose
+    private String firstname;
+    @SerializedName("middlename")
+    @Expose
+    private String middlename;
+    @SerializedName("lastname")
+    @Expose
+    private String lastname;
+    @SerializedName("rank")
+    @Expose
+    private Integer rank;
+    @SerializedName("role")
+    @Expose
+    private String role;
+    @SerializedName("organization")
+    @Expose
+    private String organization;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public void setMiddlename(String middlename) {
+        this.middlename = middlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getRank() {
+        return rank;
+    }
+
+    public void setRank(Integer rank) {
+        this.rank = rank;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+}
